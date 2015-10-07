1f364bd46531f74bc2dc17eb2c34f3d315de1e1c
Dotan Simha <dotansimha@gmail.com>
2015-10-07 20:10:58 +0300

    Step 3.6: Add the chat messages collection on the ChatDetailCtrl

--- client/scripts/controllers/chat-detail.controller.js
+++ client/scripts/controllers/chat-detail.controller.js
@@ -5,4 +5,8 @@ angular
 function ChatDetailCtrl ($scope, $stateParams) {
   var chatId = $stateParams.chatId;
   $scope.chat = $scope.$meteorObject(Chats, chatId, false);
+
+  $scope.messages = $scope.$meteorCollection(function () {
+    return Messages.find({ chatId: chatId });
+  }, false);
 }

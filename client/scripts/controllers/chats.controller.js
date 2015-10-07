ca092983e30651e3d22ee49e2eb892ccd49a6f26
Dotan Simha <dotansimha@gmail.com>
2015-10-07 22:39:05 +0300

    Step 5.2: Add logic for opening the modal using $ionicModal

--- client/scripts/controllers/chats.controller.js
+++ client/scripts/controllers/chats.controller.js
@@ -2,11 +2,27 @@ angular
   .module('Whatsapp')
   .controller('ChatsCtrl', ChatsCtrl);
 
-function ChatsCtrl ($scope) {
+function ChatsCtrl ($scope, $ionicModal) {
   $scope.chats = $scope.$meteorCollection(Chats, false);
+
+  $ionicModal.fromTemplateUrl('client/templates/new-chat.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+
+  $scope.openNewChatModal = openNewChatModal;
   $scope.remove = remove;
 
   ////////////
+
+  function openNewChatModal () {
+    $scope.modal.show();
+  }
 
   function remove (chat) {
     $scope.chats.remove(chat);

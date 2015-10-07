dd83000bc9c8df4818d3660661f8be721639f078
Dotan Simha <dotansimha@gmail.com>
2015-10-07 20:50:20 +0300

    Step 3.17: Add logic in the ChatDetailsCtrl for better experience

--- client/scripts/controllers/chat-detail.controller.js
+++ client/scripts/controllers/chat-detail.controller.js
@@ -2,8 +2,9 @@ angular
   .module('Whatsapp')
   .controller('ChatDetailCtrl', ChatDetailCtrl);
 
-function ChatDetailCtrl ($scope, $stateParams) {
+function ChatDetailCtrl ($scope, $stateParams, $ionicScrollDelegate, $timeout) {
   var chatId = $stateParams.chatId;
+  var isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
   $scope.chat = $scope.$meteorObject(Chats, chatId, false);
 
   $scope.messages = $scope.$meteorCollection(function () {
@@ -12,10 +13,35 @@ function ChatDetailCtrl ($scope, $stateParams) {
 
   $scope.data = {};
   $scope.sendMessage = sendMessage;
+  $scope.inputUp = inputUp;
+  $scope.inputDown = inputDown;
+  $scope.closeKeyboard = closeKeyboard;
 
   ///
 
   function sendMessage () {
     // TODO: Implement this logic
+  }
+
+  function inputUp () {
+    if (isIOS) {
+      $scope.data.keyboardHeight = 216;
+    }
+
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown () {
+    if (isIOS) {
+      $scope.data.keyboardHeight = 0;
+    }
+
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+
+  function closeKeyboard () {
+    // cordova.plugins.Keyboard.close();
   }
 }
